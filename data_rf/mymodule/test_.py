c876238f077802b629d13b787e335d82db7767bc
coob <koosanghwi@gmail.com>
2025-03-20 19:34:22 +0900

    ver 1.00

---
+++ data_rf/mymodule/test_.py
@@ -0,0 +1,37 @@
+import sys
+import os
+import time
+import requests
+
+import variable as v_
+
+sys.path.append('C:/my_games/' + str(v_.game_folder) + '/' + str(v_.data_folder) + '/mymodule')
+
+
+
+def go_test():
+    import numpy as np
+    import cv2
+    import pyautogui
+    import random
+
+
+    print("test")
+    cla = "one"
+
+    plus = 0
+
+
+    if cla == "one":
+        plus = 0
+    elif cla == "two":
+        plus = 960
+    elif cla == "three":
+        plus = 960 * 2
+    elif cla == "four":
+        plus = 960 * 3
+    elif cla == "five":
+        plus = 960 * 4
+    elif cla == "six":
+        plus = 960 * 5
+
