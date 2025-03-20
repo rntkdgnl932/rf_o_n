3bee2b8923f90b5e4455e2cdba3cae6188fd79f8
coob <koosanghwi@gmail.com>
2025-03-20 23:21:41 +0900

    ver 1.03

--- data_rf/mymodule/action.py
+++ data_rf/mymodule/action.py
@@ -25,11 +25,20 @@ def confirm_all(cla):
         full_path = "c:\\my_games\\rf_o_n\\data_rf\\imgs\\action\\confirm_all\\confirm_1.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(410, 500, 630, 630, cla, img, 0.85)
+        imgs_ = imgs_set_(410, 500, 700, 700, cla, img, 0.85)
         if imgs_ is not None and imgs_ != False:
             print("confirm_1", imgs_)
-            is_confirm = False
+            is_confirm = True
             click_pos_reg(imgs_.x, imgs_.y, cla)
+        else:
+            full_path = "c:\\my_games\\rf_o_n\\data_rf\\imgs\\action\\confirm_all\\move_confirm_1.PNG"
+            img_array = np.fromfile(full_path, np.uint8)
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            imgs_ = imgs_set_(410, 500, 700, 700, cla, img, 0.85)
+            if imgs_ is not None and imgs_ != False:
+                print("move_confirm_1", imgs_)
+                is_confirm = True
+                click_pos_reg(imgs_.x, imgs_.y, cla)
 
         return is_confirm
     except Exception as e:
@@ -55,7 +64,7 @@ def menu_open(cla):
         imgs_ = imgs_set_(410, 500, 550, 630, cla, img, 0.85)
         if imgs_ is not None and imgs_ != False:
             print("confirm_1", imgs_)
-            is_confirm = False
+            is_confirm = True
             click_pos_reg(imgs_.x, imgs_.y, cla)
 
         return is_confirm
@@ -157,9 +166,6 @@ def juljun_on(cla):
     from clean_screen import clean_screen_start
     try:
         print("juljun_on")
-
-
-
 
         for i in range(10):
             full_path = "c:\\my_games\\rf_o_n\\data_rf\\imgs\\game_check\\juljun\\juljun_on.PNG"
