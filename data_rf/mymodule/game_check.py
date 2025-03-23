10427b21ef78e89b3e5c6bee7fb6ddd535de76e4
coob <koosanghwi@gmail.com>
2025-03-23 21:54:38 +0900

    ver 1.0402

--- data_rf/mymodule/game_check.py
+++ data_rf/mymodule/game_check.py
@@ -37,6 +37,16 @@ def game_check_start(cla):
                 why = "장시간...."
                 line_to_me(cla, why)
                 macro_out(cla)
+            else:
+                full_path = "c:\\my_games\\rf_o_n\\data_rf\\imgs\\game_check\\new_path_notice.PNG"
+                img_array = np.fromfile(full_path, np.uint8)
+                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                imgs_ = imgs_set_(330, 500, 500, 600, cla, img, 0.85)
+                if imgs_ is not None and imgs_ != False:
+                    print("new_path_notice", imgs_)
+                    why = "새로운 패치...."
+                    line_to_me(cla, why)
+                    macro_out(cla)
 
     except Exception as e:
         print(e)
