4b347d9875848434aa4adc67bbcb57c8aebb1cf3
coob <koosanghwi@gmail.com>
2025-04-06 09:27:31 +0900

    ver 1.07308

--- data_rf/mymodule/test_.py
+++ data_rf/mymodule/test_.py
@@ -80,13 +80,12 @@ def go_test():
         #
         # print("nowMinute", nowMinute)
 
-        full_path = "c:\\my_games\\rf_o_n\\data_rf\\imgs\\clean_screen\\out_btn_1.PNG"
+        full_path = "c:\\my_games\\rf_o_n\\data_rf\\imgs\\action\\cancle_all\\cancel_btn_1.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(900, 30, 960, 200, cla, img, 0.9)
+        imgs_ = imgs_set_(300, 500, 700, 800, cla, img, 0.85)
         if imgs_ is not None and imgs_ != False:
-            clean = False
-            print("out_btn_1", imgs_)
+            print("cancel_btn_1", imgs_)
 
         # mission_get(cla)
 
