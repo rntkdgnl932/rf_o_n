1b8ee552705aca7515415b5c93734d76dcac8c89
coob <koosanghwi@gmail.com>
2025-05-02 19:45:51 +0900

    ver 1.08402

--- data_rf/mymodule/dungeon.py
+++ data_rf/mymodule/dungeon.py
@@ -18,7 +18,7 @@ def dungeon_start(cla, data):
     from massenger import line_to_me
     from function_game import imgs_set_, macro_out, click_pos_2
     from game_check import out_check, attack_check
-    from action import juljun_check, juljun_on, attack_on, juljun_off
+    from action import juljun_check, juljun_on, attack_on, juljun_off, go_random
     from clean_screen import clean_screen_start
     from potion import potion_check
     from schedule import myQuest_play_add
