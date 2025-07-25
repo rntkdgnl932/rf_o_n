8c730a87b1cd91542fbd9ee87768b6bda97bd81f
coob <koosanghwi@gmail.com>
2025-07-25 14:12:13 +0900

    ver 1.0955

--- data_rf/mymodule/test_.py
+++ data_rf/mymodule/test_.py
@@ -63,7 +63,7 @@ def go_test():
         full_path = "c:\\my_games\\rf_o_n\\data_rf\\imgs\\dungeon\\pyegijang\\title.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(400, 380, 560, 430, cla, img, 0.8)
+        imgs_ = imgs_set_(400, 380, 560, 430, cla, img, 0.7)
         if imgs_ is not None and imgs_ != False:
             print("title : pyegijang", imgs_)
 
