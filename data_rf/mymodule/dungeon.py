ae2690069c83b4455a4007f38a15f328c4e950ba
coob <koosanghwi@gmail.com>
2025-07-25 14:09:58 +0900

    ver 1.0955

--- data_rf/mymodule/dungeon.py
+++ data_rf/mymodule/dungeon.py
@@ -591,7 +591,7 @@ def dun_in_sotang(cla, data):
                             full_path = "c:\\my_games\\rf_o_n\\data_rf\\imgs\\dungeon\\sotang_title.PNG"
                             img_array = np.fromfile(full_path, np.uint8)
                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                            imgs_ = imgs_set_(440, 360, 515, 410, cla, img, 0.85)
+                            imgs_ = imgs_set_(440, 360, 515, 410, cla, img, 0.8)
                             if imgs_ is not None and imgs_ != False:
                                 print("sotang_title 1", imgs_)
                                 break
@@ -607,7 +607,7 @@ def dun_in_sotang(cla, data):
                             full_path = "c:\\my_games\\rf_o_n\\data_rf\\imgs\\dungeon\\sotang_title.PNG"
                             img_array = np.fromfile(full_path, np.uint8)
                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                            imgs_ = imgs_set_(440, 360, 515, 410, cla, img, 0.85)
+                            imgs_ = imgs_set_(440, 360, 515, 410, cla, img, 0.8)
                             if imgs_ is not None and imgs_ != False:
                                 print("sotang_title 2", imgs_)
                                 confirm_all(cla)
